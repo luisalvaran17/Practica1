2d593316e081b2cced70d1fec4e38f36c8af5a8b
SebastianDelgad <delgado.sebastian@correounivalle.edu.co>
2017-04-05 11:19:43 -0500

    falta C

---
+++ ParcialB/4.cpp
@@ -0,0 +1,36 @@
+/*
+ * Autor: Luis Albaran, Sebastian Delgado
+ * Archivo: 4B
+ * Descripción: Punto 4 del parcial B
+ * Fecha de creación: 5 de abril
+ * Fecha de modificación: 5 de abril
+*/
+
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+ void agregarEmpleado( int empleado, map<int, double> registro){
+	 
+	 for (map <int,double> ::iterator it = registro.begin (); it!= registro.end (); ++ it) {
+	    if ((it -> first) == empleado){
+			cout <<"Error"<<endl;
+		}
+	}
+		registro[empleado] = 0;
+	}
+	    
+  int main () {
+	  int empleado;
+	 
+	  
+	  cout <<"ingrese el empleado"<<endl;
+	  cin >> empleado;
+	  map <int,double> registro;
+	  registro [1234] = 5;
+	  registro [5678] = 675;
+	  
+	 agregarEmpleado (empleado, registro);
+  }
